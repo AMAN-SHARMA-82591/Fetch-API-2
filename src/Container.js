3e9a452bd5368cc3c884e54b100ff17ca9208796
Aman Sharma <aman82591@gmail.com>
2021-09-08 13:07:57 +0530

    Todo UI completed

--- src/Container.js
+++ src/Container.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import SearchIcon from '@material-ui/icons/Search';
 function Container(){
+    const [input,setInput] = useState("");
     const [user,setUser] = useState([]);
+    console.log(input);
     const getUser = async () => {
         const response = await fetch('https://jsonplaceholder.typicode.com/todos');
         
@@ -13,35 +16,37 @@ function Container(){
 
     return(
         <div>
-                {/* {
-                    user.map((i) => {
-                        return(
-                            <div key={i.id}>
-
-                            </div>
-                        )
-                    })
-                } */}
+               
                 <div className="main-container">
                     <div className="cont-text">
-                        <p style={{margin:'0px'}}>Todos</p>
-                        <div>
-                            <button>S</button>
-                            <input placeholder="Search"></input>
+                        <h2 style={{margin:'0px'}}>Todos</h2>
+                        <div className="search-bar-cont">
+                            <button><SearchIcon fontSize="small" /></button>
+                            <input onChange={(e) => setInput(e.target.value)} placeholder="Search"></input>
                         </div>
                     </div>
                     <table className="todo-table">
-                        <tr>
+                        <tr className="todo-main-cont">
                             <th>Todo Id</th>
                             <th>Title</th>
                             <th>Status</th>
                             <th>Action</th>
                         </tr>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            <td></td>
+                        
+                        <tr className="todo-cont-item">
+                            {
+                                user.map((todo) =>{
+                                    return(
+                                        <React.Fragment key={todo.id}>
+                                            <td style={{paddingLeft:"15px"}}>{todo.id}</td>
+                                            <td style={{paddingRight:"15px"}}>{todo.title}</td>
+                                            <td>{todo.completed === true?"Complete":"Incomplete"}</td>
+                                            <td className="todo-view-cont"><button>View User</button></td>
+                                           
+                                        </React.Fragment>
+                                    )
+                                })
+                            }
                         </tr>
                     </table>
                 </div>
